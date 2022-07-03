e3450bc20259b275fd3164878473327f50bb53b7
Matthias Thul <matthias.thul@protonmail.com>
2022-07-04 00:30:17 +0200

    Solve assignment

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -1,7 +1,8 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import "truffle/Assert.sol";
-import "truffle/DeployedAddresses.sol";
+// import "truffle/Assert.sol";
+// import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
